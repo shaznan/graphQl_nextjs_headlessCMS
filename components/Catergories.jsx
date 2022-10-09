a8e09229fc9d99719787ff06dd10a3d30ff4bec5
Shaznan <shaznanfairozesally@gmail.com>
2022-10-09 17:05:23 +0530

    Install Tailwind, initiate components

---
+++ components/Catergories.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Catergories = () => {
+  return <div>Catergories</div>;
+};
+
+export default Catergories;
--- pages/index.js
+++ pages/index.js
@@ -13,18 +13,23 @@ const posts = [
 
 export default function Home() {
   return (
-    <div className="container mx-auto px-10 mb-8 bg-slate-400">
+    <div className="max-w-7xl mx-auto mb-8 px-10 ">
       <Head>
         <title>CMS Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        {posts?.map((post, index) => (
-          <div key={index}>
-            {post.title}
-            {post.exerpt}
-          </div>
-        ))}
+        <div className="lg:col-span-8 col-span-1">
+          {posts?.map((post, index) => (
+            <div key={index}>
+              {post.title}
+              {post.exerpt}
+            </div>
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8"></div>
+        </div>
       </div>
     </div>
   );
