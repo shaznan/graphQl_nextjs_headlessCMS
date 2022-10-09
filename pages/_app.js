2c9399dd84c74165d242bdb786d6bb792fdb9f49
Shaznan <shaznanfairozesally@gmail.com>
2022-10-09 15:50:10 +0530

    WIP

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,7 @@
 import "../styles/globals.css";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-
-export const client = new ApolloClient({
-  uri: "https://api-us-west-2.hygraph.com/v2/cl8k1zu6q18pe01td4a3pduq9/master",
-  cache: new InMemoryCache(),
-});
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <ApolloProvider client={client}>
-      <Component {...pageProps} />
-    </ApolloProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
