4c30339a864c33f2122fb0f3456bc5cd3f223e83
Shaznan <shaznanfairozesally@gmail.com>
2022-10-13 10:05:56 +0530

    Add Query

--- services/index.js
+++ services/index.js
@@ -1,3 +1,33 @@
 import { request, gql } from "graphql-request";
 
-export const getPosts = () => {};
+export const getPosts = async () => {
+  const query = gql`
+    query MyQuery {
+      postsConnection {
+        edges {
+          node {
+            createdAt
+            slug
+            title
+            exerpt
+            author {
+              bio
+              name
+              id
+              photo {
+                url
+              }
+            }
+            featuredImage {
+              url
+            }
+            catergories {
+              name
+              slug
+            }
+          }
+        }
+      }
+    }
+  `;
+};
