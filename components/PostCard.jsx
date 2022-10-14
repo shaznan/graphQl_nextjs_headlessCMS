a6dbe73a755e6c74f8c2bb3bb8f9cb3fcbd2d786
Shaznan <shaznanfairozesally@gmail.com>
2022-10-14 23:22:00 +0530

    Work on post card component

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,7 +1,31 @@
+import Link from "next/link";
 import React from "react";
 
-const PostCard = () => {
-  return <div>PostCard</div>;
+const PostCard = ({ post }) => {
+  return (
+    <div className="bg-white show-lg rounded-lg p-0 lg:p-8 pb-12 mb-8">
+      <div className="relative overflow-hidden shadow-md pb-80 mb-6">
+        <img
+          src={post?.featuredImage?.url}
+          alt={post?.title}
+          className="object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg"
+        />
+      </div>
+      <h1 className="transition duration-700 text-center mb-8 cursor-pointer hover:text-pink-600 text-3xl font-semibold">
+        <Link href={`/post/${post?.slug}`}>{post?.title}</Link>
+      </h1>
+      <div className="block lg:flex text-center items-center justify-center mb-8 w-full ">
+        <div className="flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8">
+          <img
+            alt={post?.author?.name}
+            height="30px"
+            width="30px"
+            className="align-middle rounder-full"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PostCard;
