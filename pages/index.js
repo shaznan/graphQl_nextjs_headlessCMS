494221634268b62edc655599d4f601507441439b
Shaznan <shaznanfairozesally@gmail.com>
2022-10-14 23:22:23 +0530

    Fix bugs on accessing posts

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,6 @@ const posts = [
 ];
 
 export default function Home({ posts }) {
-  console.log(posts, "posts");
   return (
     <div className="max-w-7xl mx-auto mb-8 px-10 ">
       <Head>
@@ -24,7 +23,7 @@ export default function Home({ posts }) {
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts?.map((post, index) => (
-            <PostCard post={post} key={post.title} />
+            <PostCard post={post?.node} key={post.title} />
           ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
