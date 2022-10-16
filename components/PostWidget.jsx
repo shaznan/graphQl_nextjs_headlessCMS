bb948b8d228a8849724812776bea5cebc6fe4d6b
Shaznan <74103414+shaznan@users.noreply.github.com>
2022-10-16 23:34:33 +0100

    Make changes to next config file

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,19 +1,52 @@
-import React, {useState, useEffect} from "react";
+import moment from "moment/moment";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState, useEffect } from "react";
 import { getRecentPost, getSimilarPost } from "../services";
 
-const PostWidget = ({catergories, slug}) => {
-  const [relatedPosts, setRelatedPosts] = useState([])
+const PostWidget = ({ catergories, slug }) => {
+  const [relatedPosts, setRelatedPosts] = useState([]);
 
   useEffect(() => {
-    if(slug){
-      getSimilarPosts(catergories, slug).then((result)=> setRelatedPosts(result))
+    if (slug) {
+      getSimilarPosts(catergories, slug).then((result) =>
+        setRelatedPosts(result)
+      );
     } else {
-      getRecentPost().then((result)=> setRelatedPosts(result))
+      getRecentPost().then((result) => setRelatedPosts(result));
     }
-  }, [slug])
-  
+  }, [slug]);
 
-  return <div>PostWidget</div>;
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+        {slug ? "Related Posts" : "Recent Posts"}
+      </h3>
+      {relatedPosts.map((post, index) => (
+        <div key={index} className="flex items-center w-full mb-4">
+          <div className="w-16 flex-none">
+            <Image
+              // loader={grpahCMSImageLoader}
+              alt={post.title}
+              height="60px"
+              width="60px"
+              // unoptimized
+              className="align-middle rounded-full"
+              src={post.featuredImage.url}
+            />
+          </div>
+          <div className="flex-grow ml-4">
+            <p className="text-gray-500 font-xs">
+              {moment(post.createdAt).format("MMM DD, YYYY")}
+            </p>
+            <Link href={`/post/${post.slug}`} className="text-md" key={index}>
+              {post.title}
+            </Link>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default PostWidget;
