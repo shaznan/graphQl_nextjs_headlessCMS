0172bbf970a2d566ffb202e84f4110e8525beeda
Shaznan <74103414+shaznan@users.noreply.github.com>
2022-10-18 22:44:57 +0530

    Work on Post [slug] component

---
+++ components/Author.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Author = () => {
+  return <h1>Author</h1>;
+};
+
+export default Author;
---
+++ pages/post/[slug].js
@@ -0,0 +1,43 @@
+import React from "react";
+import { getPosts, getPostDetails } from "../../services";
+import {
+  PostDetail,
+  Catergories,
+  PostWidget,
+  Author,
+  Comments,
+  CommentsForm,
+} from "../../components/index";
+
+const PostDetails = () => {
+  return (
+    <>
+      <div className="container mx-auto px-10 mb-8">
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+          <div className="col-span-1 lg:col-span-8">
+            <PostDetail />
+            <Author />
+            <CommentsForm />
+            <Comments />
+          </div>
+          <div className="col-span-1 lg:col-span-4">
+            <div className="relative lg:sticky top-8">
+              <PostWidget />
+              <Catergories />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PostDetails;
+
+export const getStaticProps = async () => {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: { posts },
+  };
+};
